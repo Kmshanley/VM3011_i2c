cd6a196ae3bb15b72d7874aa01788a5b2e8eb296
Kmshanley <kmshanley01@gmail.com>
2022-07-07 14:01:57 -0400

    Fix wrong type
    The previous commit contained a error with the wrong type in the parameter for the free description function.

--- VM3011_i2c.c
+++ VM3011_i2c.c
@@ -20,7 +20,7 @@ esp_err_t VM3011_init_desc(VM3011_t *dev, uint8_t addr, i2c_port_t port, gpio_nu
     return i2c_dev_create_mutex(&dev->i2c_dev);
 }
 
-esp_err_t VM3011_free_desc(bme680_t *dev)
+esp_err_t VM3011_free_desc(VM3011_t *dev)
 {
     return i2c_dev_delete_mutex(&dev->i2c_dev);
 }
